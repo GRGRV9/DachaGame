9cfd8aaf6935a973adb03b9d27c611101c8fc9d4
GRGRV9 <niamod@mail.ru>
2023-11-05 19:46:57 +0600

    update enemyspawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
-    public GameObject enemyPrefab;
+    public GameObject enemy1Prefab;
+    public GameObject enemy2Prefab;
+    public GameObject enemy3Prefab;
+
     private Quaternion spawnPosition;
 
-    public void SpawnEnemy()
+    public void SpawnEnemy1()
     {
         spawnPosition = Quaternion.Euler(new Vector3(0, 0, Random.Range(-40, 40)));
-        Debug.Log(spawnPosition); 
-        Instantiate(enemyPrefab, new Vector3(0, 0, 0), spawnPosition);
+        Instantiate(enemy1Prefab, new Vector3(0, 0, 0), spawnPosition);
     }
-    
-
+    public void SpawnEnemy2()
+    {
+        spawnPosition = Quaternion.Euler(new Vector3(0, 0, Random.Range(-40, 40)));
+        Instantiate(enemy2Prefab, new Vector3(0, 0, 0), spawnPosition);
+    }
+    public void SpawnEnemy3()
+    {
+        spawnPosition = Quaternion.Euler(new Vector3(0, 0, Random.Range(-40, 40)));
+        Instantiate(enemy3Prefab, new Vector3(0, 0, 0), spawnPosition);
+    }
 }
